0c80a1a43cab496deeffbffb71c61f9097dc24ce
Maria Piper <mbtomori@gmail.com>
2014-08-15 17:05:14 -0700

    added titles for each mission

--- week-3/1-code-combat/my_solution.js
+++ week-3/1-code-combat/my_solution.js
@@ -1,16 +1,23 @@
 // I worked on this challenge [by myself, with:]
- 
-// For each mission, write the title as a comment. (Shown here). Also include pseudocode as a comment. 
-// Note: to make commenting easier, you can highlight the section you want to comment and hold 
-// command + / This will comment the line. 
- 
- 
- 
- 
- 
- 
- 
- 
- 
+
+// Write pseudocode as a comment for each mission.
+// Note: to make commenting easier, you can highlight the section you want to comment and hold
+// command + / This will comment the line.
+
+
+// 1. Rescue Mission
+
+
+
+
+// 2. Grab the Mushroom
+
+
+
+
+// 3. Drink Me
+
+
+
 // Reflection:
 // Write your reflection here.
